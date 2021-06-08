a333637db6213d34a480c3e493bd4ebf68006475
mameGG <r.takahashi@april-knights.com>
2021-06-08 21:04:14 +0900

    switch文

--- practice/5-02_location/script.js
+++ practice/5-02_location/script.js
@@ -13,3 +13,23 @@ if (lang === 'ja') {
 } else if (lang === 'zh') {
   document.querySelector('option[value="index-zh.html"]').selected = true;
 } 
+// querySelector('option')とかで複数要素を取得した際は、最初の要素のみ取得する。
+// ＝つまり「複数同時に取得する」メソッドではない。
+
+// ===の左側が全て同一（ここだとlang）であればswitch文で以下のように書き換えられる。
+// switch文で書けるのであればif文でも書けるので、どちらが適切か選んで使う
+
+// switch(lang) {
+//   case 'ja' :
+//     document.querySelector('option[value="index.html"]').selected = true;
+//     break;
+//   case 'en' :
+//     document.querySelector('option[value="index-en.html"]').selected = true;
+//     break;
+//   case 'zh' :
+//     document.querySelector('option[value="index-zh.html"]').selected = true;
+//     break;
+// }
+
+
+
